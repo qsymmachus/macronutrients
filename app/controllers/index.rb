c949cbe36a5bae9ff4cc4ae91bf9cbd8f55814b3
John Olmsted <johnaolmsted@gmail.com>
2014-02-15 18:28:36 -0800

    Disables background jobs for heroku deployment

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,9 +8,13 @@ get '/foods' do
   results_number = params[:results_number].to_i
   @foods = NutritionGetter.first_x_results(food, results_number)
   @search_term = food
-  if results_number == 5 && !NutritionGetter.food_in_local_database?(food)
-    DatabaseWorker.perform_async(params[:food])
-  end
+
+  # Disables redis/sidekiq background job for heroku deployment:
+
+  # if results_number == 5 && !NutritionGetter.food_in_local_database?(food)
+  #   DatabaseWorker.perform_async(params[:food])
+  # end
+ 
   erb :index
 end
 
